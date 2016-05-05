ef381b8417613562e6ce8396b2e0404b6f35af89
TomoProg <helloworld0306.xxx@gmail.com>
2016-05-05 15:12:12 +0900

    make model commit

--- python/django/PyBoard/mysite/PyBoard/admin.py
+++ python/django/PyBoard/mysite/PyBoard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic, TopicDetail
 
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(TopicDetail)
+
--- python/django/PyBoard/mysite/PyBoard/models.py
+++ python/django/PyBoard/mysite/PyBoard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """ Topic Table """
+    title = models.CharField(max_length=500)
+    register_name = models.CharField(max_length=20)
+    register_datetime = models.DateTimeField()
+    update_name = models.CharField(max_length=20)
+    update_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class TopicDetail(models.Model):
+    """ TopicDetail Table """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    register_name = models.CharField(max_length=20)
+    register_datetime = models.DateTimeField()
+    update_name = models.CharField(max_length=20)
+    update_datetime = models.DateTimeField()
+
